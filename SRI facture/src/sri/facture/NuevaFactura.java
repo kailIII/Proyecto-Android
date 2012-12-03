542c2e27a6afc77051be13d0ed200fd1479af7d4
rxcampoz <rxcampoz@gmail.com>
2012-12-03 01:56:51 -0500

    Cambio en nueva factura: eliminacion de campos innecesarios

--- SRI facture/src/sri/facture/NuevaFactura.java
+++ SRI facture/src/sri/facture/NuevaFactura.java
@@ -3,6 +3,10 @@
  */
 package sri.facture;
 
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 import sri.facture.bd.DatabaseHelper;
 import sri.facture.providers.DeducibleProvider;
 import sri.facture.providers.FacturaProvider;
@@ -11,6 +15,8 @@ import sri.facture.providers.UsuarioProvider;
 import com.sri.facture.R;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
@@ -19,6 +25,8 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -34,7 +42,25 @@ public class NuevaFactura extends Activity {
     String tsalud="0"; 
     String tvestimenta="0"; 
     String tvivienda="0"; 
-	
+    
+    private TextView mDateDisplay;    
+    private Button mPickDate;    
+    private int mYear;    
+    private int mMonth;    
+    private int mDay;    
+    static final int DATE_DIALOG_ID = 0;
+    
+ // the callback received when the user "sets" the date in the dialog    
+    private DatePickerDialog.OnDateSetListener mDateSetListener =            
+    	new DatePickerDialog.OnDateSetListener() {                
+    	public void onDateSet(DatePicker view, int year,                                       
+    			int monthOfYear, int dayOfMonth) {                    
+    		mYear = year;                    
+    		mMonth = monthOfYear;                    
+    		mDay = dayOfMonth;                    
+    		updateDisplay();                
+    		}            
+    	};
 	@Override
 	    public void onCreate(Bundle savedInstanceState) {
 	        super.onCreate(savedInstanceState);
@@ -45,16 +71,36 @@ public class NuevaFactura extends Activity {
 	        	id_user=extras.getString("id_user");                        
 	        }
 	        
+	     // capture our View elements        
+	    	mDateDisplay = (TextView) findViewById(R.id.dateDisplay);        
+	    	mPickDate = (Button) findViewById(R.id.pickDate);        
+	    	// add a click listener to the button        
+	    	mPickDate.setOnClickListener(new View.OnClickListener() 
+	    		{            
+	    		public void onClick(View v) {                
+	    			showDialog(DATE_DIALOG_ID);            
+	    			}        
+	    		});        
+	    	// get the current date        
+	    	final Calendar c = Calendar.getInstance();        
+	    	mYear = c.get(Calendar.YEAR);        
+	    	mMonth = c.get(Calendar.MONTH);        
+	    	mDay = c.get(Calendar.DAY_OF_MONTH);        
+	    	// display the current date (this method is below)        
+	    	updateDisplay();    
+	        	        
 	    }
 	 
 	 	public void Save(View view){
 	 		String numero=((EditText) findViewById(R.id.numero)).getText().toString();
-	 		String fecha="2012/05/12";
+	 		String fecha=((TextView) findViewById(R.id.dateDisplay)).getText().toString();
+	 		Log.d("fecha", "aqui la fecha: "+fecha);
+	 		
 	 		String tgasto=((EditText) findViewById(R.id.gasto)).getText().toString();
 	 		//String tdeducible=((EditText) findViewById(R.id.deducible)).getText().toString();
-	 		String ciudad=((EditText) findViewById(R.id.ciudad)).getText().toString();
+	 		//String ciudad=((EditText) findViewById(R.id.ciudad)).getText().toString();
 	 		String rucp=((EditText) findViewById(R.id.rprov)).getText().toString();
-	 		String prov=((EditText) findViewById(R.id.prov)).getText().toString();
+	 		//String prov=((EditText) findViewById(R.id.prov)).getText().toString();
 	 		//insert into factura values('1','22','22','2007/05/12','333','44','ciud','nlo','1');
 	 		if(numero.trim().isEmpty()){
 				Toast.makeText(this,"Faltan campos por completar", Toast.LENGTH_SHORT).show();
@@ -69,11 +115,11 @@ public class NuevaFactura extends Activity {
     			values.put(
     					FacturaProvider.TOTAL_GASTO,tgasto);
     			values.put(
-    					FacturaProvider.CIUDAD,ciudad);
+    					FacturaProvider.CIUDAD,"");
     			values.put(
     					FacturaProvider.RUC_PROVEEDOR,rucp);
     			values.put(
-    					FacturaProvider.N_PROVEEDOR,prov);
+    					FacturaProvider.N_PROVEEDOR,"");
     			values.put(
     					FacturaProvider.ID_USUARIO,id_user);
     			
@@ -168,7 +214,30 @@ public class NuevaFactura extends Activity {
 	            }
 	    }
 	 	
-	 	
+	 // updates the date in the TextView    
+	    private void updateDisplay() {        
+	    	mDateDisplay.setText(            
+	    			new StringBuilder()
+	    			.append(mYear).append("/")
+	    			// Month is 0 based so add 1 
+	    			.append(mMonth + 1).append("/") 
+	    			.append(mDay).append(" "));   
+	    			                   
+	    			                                  
+	    			  
+	    }
+	    
+	    
+	    @Override
+	    protected Dialog onCreateDialog(int id) {    
+	    	switch (id) {   
+	    		case DATE_DIALOG_ID:        
+	    			return new DatePickerDialog(this,                    
+	    					mDateSetListener,                    
+	    					mYear, mMonth, mDay);    
+	    			}    
+	    	return null;
+	    	}
 	 	
 	 	
 	 	
